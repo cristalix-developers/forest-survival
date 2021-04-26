67e7374224f404aeb474d2b705191710fb1b7c47
delfikpro <33707322+DelfikPro@users.noreply.github.com>
2021-04-27 02:24:16 +0300

    Update project structure

--- forest-image/src/main/groovy/me.func.script/Test.groovy
+++
@@ -1,8 +0,0 @@
-@groovy.transform.BaseScript(me.func.forest.ScriptSupport)
-package me.func.script
-
-import org.bukkit.event.player.PlayerJoinEvent
-
-on PlayerJoinEvent, {
-    player.sendMessage("Hi")
-}
